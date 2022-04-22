2c6766b476087540f5e222751af5368f16d180d3
djodre <jodre.diangienda@gmail.com>
2022-04-22 11:46:16 +0200

    Initial commit

---
+++ app/src/main/java/com/example/td_fragment_kotlin_20220422/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.td_fragment_kotlin_20220422
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
